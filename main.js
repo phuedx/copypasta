b1c90794b4f710b64df7e8b0f4b29029b9028cf3
Sam Smith <git@samsmith.io>
2017-01-22 15:13:06 +0000

    Add logging
    As a packaged application doesn't have a tty to log to and the sound
    played by electron.shell#beep is subtle, log to a tty, if one is
    available, and a file.

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const { app, clipboard, shell } = require('electron')
-const observableClipboard = require('./src/observable-clipboard')
+const log = require('electron-log')
 
+const observableClipboard = require('./src/observable-clipboard')
 const doodads = require('./src/doodads')
 
 app.on('ready', () => {
+  log.info('Ready...')
+
   observableClipboard.subscribe({
     next (text) {
+      log.info(`Trying to find a doodad for "${text}"...`)
+
       let doodad
 
       for (let i = 0; i < doodads.length; ++i) {
@@ -17,11 +22,14 @@ app.on('ready', () => {
       }
 
       if (doodad) {
+        log.info('Found a doodad!')
+        log.info('Doodading...')
+
         doodad.doodad(text)
           .then(clipboard.writeHTML)
           .then(shell.beep)
       } else {
-        console.log(`Can't find a doodad that can doodad "${text}"`)
+        log.info(`Couldn't find a doodad for "${text}"`)
       }
     }
   })
