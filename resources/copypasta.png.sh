0396cb0dbcfeb6829ed708c625251cde13fea8b1
Sam Smith <git@samsmith.io>
2018-06-20 16:10:17 +0100

    feat: Add an icon
    This is preparatory work for adding an icon and context menu to the
    system's notification area.
    
    Use the clipboard icon from the Feather icon collection, converting it
    to an 18x18 PNG with rsvg-convert from librsvg2-bin.

---
+++ resources/copypasta.png.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+__dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+node -e "\
+  const { writeFileSync } = require('fs');\
+  const feather = require('feather-icons');\
+  \
+  writeFileSync('${__dirname}/copypasta.svg', feather.icons.clipboard.toSvg({\
+    width: 18, height: 18,\
+    color: 'white'\
+  }));"
+
+# Equivalent to
+#
+#   rsvg-convert --format=png --output="$__dirname/copypasta.png" "$__dirname/copypasta.svg"
+rsvg-convert "$__dirname/copypasta.svg" > "$__dirname/copypasta.png"
+
+rm "$__dirname/copypasta.svg"
