8996bd529ff4341270b9f93ef20bc28c1dd7ebb5
Sam Smith <git@samsmith.io>
2018-06-20 16:45:17 +0100

    feat: Add icon and context menu to the tray
    The context menu provides:
    
    - The ability to quit the application.
    
    - A history of the pieces of copied plaintext that were modified by the
      application as menu items, which, when clicked, will trigger the
      application re-modify that plaintext and copy it to the clipboard.

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-const { app, clipboard, shell } = require('electron')
-
+const { app, clipboard, shell, Menu, Tray, MenuItem } = require('electron')
+const { resolve } = require('path')
 const observableClipboard = require('./src/observable-clipboard')
 const log = require('./src/log')
 const doodads = require('./src/doodads')
+
+let tray
+const menu = Menu.buildFromTemplate([
+  { type: 'separator' },
+  { role: 'quit' }
+])
 
 /**
  * Notifies the user that the clipboard's text has been modified.
@@ -13,33 +19,69 @@ function notifyUser () {
   app.dock.bounce('informational')
 }
 
+/**
+ * Try to find a doodad that can convert a piece of text to HTML in some way
+ * and, if so, uses it and alerts the user to it.
+ *
+ * @param {string} text
+ * @return {Promise<void>} If a doodad is found, then a resolved promise is
+ *  returned; a rejected promise is returned otherwise.
+ */
+function tryCopypasta (text) {
+  log.info(`Trying to find a doodad for "${text}"...`)
+
+  let doodad
+
+  for (let i = 0; i < doodads.length; ++i) {
+    if (doodads[i].can(text)) {
+      doodad = doodads[i]
+
+      break
+    }
+  }
+
+  if (doodad) {
+    log.info('Found a doodad!')
+    log.info('Doodading...')
+
+    return doodad.doodad(text)
+      .then(clipboard.writeHTML)
+      .then(notifyUser)
+  }
+
+  const message = `Couldn't find a doodad for "${text}"`
+
+  log.info(message)
+
+  return Promise.reject(new Error(message))
+}
+
 app.on('ready', () => {
   log.info('Ready...')
 
+  tray = new Tray(resolve('./resources/copypasta.png'))
+  tray.setToolTip('copypasta')
+  tray.setContextMenu(menu)
+
   observableClipboard.subscribe({
     next (text) {
-      log.info(`Trying to find a doodad for "${text}"...`)
+      tryCopypasta(text)
+        .then(() => {
+          menu.insert(0, new MenuItem({
+            label: text,
+            click: () => tryCopypasta(text)
+          }))
 
-      let doodad
-
-      for (let i = 0; i < doodads.length; ++i) {
-        if (doodads[i].can(text)) {
-          doodad = doodads[i]
-
-          break
-        }
-      }
-
-      if (doodad) {
-        log.info('Found a doodad!')
-        log.info('Doodading...')
-
-        doodad.doodad(text)
-          .then(clipboard.writeHTML)
-          .then(notifyUser)
-      } else {
-        log.info(`Couldn't find a doodad for "${text}"`)
-      }
+          // Per Electron's documentation [0]:
+          //
+          // > On Linux in order for changes made to individual MenuItems to
+          // > take effect, you have to call setContextMenu again.
+          //
+          // This affects macOS High Sierra (10.13.5) too.
+          //
+          // [0] https://electronjs.org/docs/all#class-tray
+          tray.setContextMenu(menu)
+        })
     }
   })
 })
