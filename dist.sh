8cac3461501082a2bf68c7ae0b1eda059e471008
Sam Smith <git@samsmith.io>
2018-06-21 11:57:46 +0100

    fix: Make dist script repeatable
    ... and fix the following style issues:
    
    - Fix SC2103: Use a ( subshell ) to avoid having to cd back.
    
    - Always use the ${foo} syntax for string interpolation.

--- dist.sh
+++ dist.sh
@@ -2,20 +2,27 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-echo "==> Packaging app..."
+__dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "${__dirname}/dist"
+
+echo "==> Cleaning up..."
+
+rm ./*.tar.gz* >/dev/null 2>&1 # Remove all archives and signatures.
+
+echo "==> Packaging..."
+
 npm run dist:prod
 
-cd dist
-
 echo "==> Compressing and signing packages..."
+
 for package in *; do
-  echo "===> $package..."
+  echo "===> ${package}..."
 
   archive_file="${package}.tar.gz"
 
-  tar -cz --options gzip:compression-level=9 -f "$archive_file" "$package"
-
-  gpg --detach-sig --output "${archive_file}.sig" "$archive_file"
+  tar -cz --options gzip:compression-level=9 -f "${archive_file}" "${package}"
+  gpg --detach-sig --output "${archive_file}.sig" "${archive_file}"
 done
 
-cd .. # cd dist
+popd >/dev/null 2>&1 # pushd ${__dirname}
